42dd4cd1470b46c1fcefa60f1419c8f5343c5634
Ori Hoch <ori@uumpa.com>
2020-04-15 20:40:29 +0300

    github_pull_covid19_israel: config user email and name

--- common/github_pull_covid19_israel.py
+++ common/github_pull_covid19_israel.py
@@ -9,6 +9,8 @@ def flow(*_):
     logging.info('Pulling latest code from COVID19-ISRAEL github repo')
     logging.info('COVID19_ISRAEL_REPOSITORY=%s' % os.environ.get('COVID19_ISRAEL_REPOSITORY'))
     logging.info('pulling from origin/master')
+    utils.subprocess_call_log(['git', 'config', 'user.email', 'avid-covider-pipelines@localhost'], cwd='../COVID19-ISRAEL')
+    utils.subprocess_call_log(['git', 'config', 'user.name', 'avid-covider-pipelines'], cwd='../COVID19-ISRAEL')
     if utils.subprocess_call_log(['git', 'pull', 'origin', 'master'], cwd='../COVID19-ISRAEL') != 0:
         raise Exception('Failed to git pull')
     sha1 = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd='../COVID19-ISRAEL').decode().strip()
