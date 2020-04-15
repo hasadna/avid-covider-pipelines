e5e3440ffb18a8b17a76c21e2c16fd6143012346
Ori Hoch <ori@uumpa.com>
2020-04-15 21:29:31 +0300

    fix html escaping bug in dpp dashboard

--- common/utils.py
+++ common/utils.py
@@ -1,10 +1,12 @@
 import logging
 import subprocess
+from html import escape
 
 
 def subprocess_call_log(*args, **kwargs):
     with subprocess.Popen(*args, **kwargs, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
         for line in iter(proc.stdout.readline, b''):
-            logging.info(line.decode().rstrip())
+            # html escape is needed due to bug in dpp dashboard
+            logging.info(escape(line.decode().rstrip()))
         proc.wait()
         return proc.returncode
