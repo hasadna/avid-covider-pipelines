37584075bbd40177e9fb3887edec2407eefd3fd6
Ori Hoch <ori@uumpa.com>
2020-04-17 12:45:07 +0300

    corona_data_collector: fix output data

--- avid_covider_pipelines/corona_data_collector_flow.py
+++ avid_covider_pipelines/corona_data_collector_flow.py
@@ -9,7 +9,7 @@ OUTPUT_DIR = 'data/corona_data_collector'
 
 def corona_data_collector_main(last_run_row, run_row):
     os.makedirs('%s/destination_output' % OUTPUT_DIR, exist_ok=True)
-    for k, v in main({'source': 'db'}):
+    for k, v in main({'source': 'db'}).items():
         run_row[k] = v
     return run_row
 
