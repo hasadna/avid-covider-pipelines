555c605f7f7bc8bc80af0c7e3fc3b37167bf225a
Ori Hoch <ori@uumpa.com>
2020-04-18 12:59:31 +0300

    add support for switching COVID19-ISRAEL branch (for testing)

--- avid_covider_pipelines/github_pull_covid19_israel.py
+++ avid_covider_pipelines/github_pull_covid19_israel.py
@@ -9,11 +9,22 @@ import subprocess
 def flow(parameters, *_):
     logging.info('Pulling latest code from COVID19-ISRAEL github repo')
     logging.info('COVID19_ISRAEL_REPOSITORY=%s' % os.environ.get('COVID19_ISRAEL_REPOSITORY'))
-    logging.info('pulling from origin/master')
+    logging.info('COVID19_ISRAEL_BRANCH=%s' % os.environ.get('COVID19_ISRAEL_BRANCH'))
     utils.subprocess_call_log(['git', 'config', 'user.email', 'avid-covider-pipelines@localhost'], cwd='../COVID19-ISRAEL')
     utils.subprocess_call_log(['git', 'config', 'user.name', 'avid-covider-pipelines'], cwd='../COVID19-ISRAEL')
-    if utils.subprocess_call_log(['git', 'pull', 'origin', 'master'], cwd='../COVID19-ISRAEL') != 0:
-        raise Exception('Failed to git pull')
+    branch = os.environ.get('COVID19_ISRAEL_BRANCH')
+    if branch:
+        logging.info('Pulling from origin/' + branch)
+        if utils.subprocess_call_log(['git', 'fetch', 'origin'], cwd='../COVID19-ISRAEL') != 0:
+            raise Exception('Failed to fetch origin')
+        if utils.subprocess_call_log(['git', 'checkout', branch], cwd='../COVID19-ISRAEL') != 0:
+            raise Exception('Failed to switch branch')
+        if utils.subprocess_call_log(['git', 'pull', 'origin', branch], cwd='../COVID19-ISRAEL') != 0:
+            raise Exception('Failed to git pull')
+    else:
+        logging.info('pulling from origin/master')
+        if utils.subprocess_call_log(['git', 'pull', 'origin', 'master'], cwd='../COVID19-ISRAEL') != 0:
+            raise Exception('Failed to git pull')
     sha1 = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd='../COVID19-ISRAEL').decode().strip()
     return Flow(
         iter([{'sha1': sha1}]),
